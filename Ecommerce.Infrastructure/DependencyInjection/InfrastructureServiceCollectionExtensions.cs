ab06df68d8dc62da43d20cee02d811fd48351681
Adriano-Guedes <adrianno.aguedes@g,ail.com>
2026-08-19 17:04:45 -0300

    Configuração da classe program e criação dos appsettings individuais

---
+++ Ecommerce.Infrastructure/DependencyInjection/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.DependencyInjection;
+
+public static class InfrastructureServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<EcommerceDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        return services;
+    }
+}
