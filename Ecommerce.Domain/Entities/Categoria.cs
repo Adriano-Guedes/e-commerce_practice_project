2ce644e76b9e169f6713963dbbb278a816d1a64f
Adriano-Guedes <adrianno.aguedes@g,ail.com>
2026-08-19 00:40:57 -0300

    Instalação de pacotes e configuração do DbContext

---
+++ Ecommerce.Domain/Entities/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ecommerce.Domain.Entities;
+
+public partial class Categoria
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public string? Descricao { get; set; }
+
+    public DateTime DataCriacao { get; set; }
+
+    public DateTime? DataAtualizacao { get; set; }
+
+    public virtual ICollection<Produto> Produtos { get; set; } = new List<Produto>();
+}
---
+++ Ecommerce.Domain/Entities/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ecommerce.Domain.Entities;
+
+public partial class Usuario
+{
+    public Guid Id { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Senha { get; set; } = null!;
+
+    public string Papel { get; set; } = null!;
+
+    /// <summary>
+    /// Opcional / Único se preenchido
+    /// </summary>
+    public string? Cpf { get; set; }
+
+    /// <summary>
+    /// Referência do arquivo no S3
+    /// </summary>
+    public string? NomeArquivoImagem { get; set; }
+
+    public DateTime DataCriacao { get; set; }
+
+    public DateTime? DataAtualizacao { get; set; }
+
+    public virtual ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+}
