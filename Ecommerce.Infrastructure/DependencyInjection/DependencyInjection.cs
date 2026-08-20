eace9c581726a16b5f88a99ef748f43ac3d977d9
Adriano-Guedes <adrianno.aguedes@g,ail.com>
2026-08-19 23:18:24 -0300

    (Parcial) Configuração da classe de injeção de dependência

---
+++ Ecommerce.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<EcommerceDbContext>(options => options.UseNpgsql(connectionString));
+
+            //services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+
+            return services;
+        }
+    }
+}
