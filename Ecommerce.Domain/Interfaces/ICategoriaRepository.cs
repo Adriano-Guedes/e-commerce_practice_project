9507b0c0d9d6d3d34e26ceaea5b8f228c3b34155
NespoliVH <vhnespoli321@gmail.com>
2026-08-20 00:30:18 -0300

    [Parcial] criação da interface ICategoriaRepository e parcial da classe CategoriaRepository

---
+++ Ecommerce.Domain/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        Task<IEnumerable<Categoria>> GetAllAsync();
+        Task<Categoria> GetByIdAsync(int id);
+        Task<bool> GetByNameAsync(string name);
+        Task<Categoria> AddAsync(Categoria categoria);
+        Task<Categoria> UpdateAsync(Categoria categoria);
+        void DeleteAsync(int id);
+    }
+}
---
+++ Ecommerce.Domain/Repositories/CategoriaRepository.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        //private readonly EcommerceDbContext _context;
+        public Task<Categoria> AddAsync(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Categoria>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Categoria> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Categoria> UpdateAsync(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
